8556acef472c1bc988db0a075bc037550d753313
Paus Haus <48162083+PDunCanCode@users.noreply.github.com>
2020-02-24 19:13:37 -0800

    Add files via upload

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,9 +1,37 @@
 import React from "react";
+import { server } from "../../lib/api";
+import { ListingsData } from "./types";
+
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`;
 
 interface Props {
   title: string;
 }
 
 export const Listings = ({ title }: Props) => {
-  return <h2>{title}</h2>;
+  const fetchListings = async () => {
+    const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
+    console.log(data); // check the console to see the listings data from our GraphQL Request!
+  };
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <button onClick={fetchListings}>Query Listings!</button>
+    </div>
+  );
 };
