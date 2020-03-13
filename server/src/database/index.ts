0f3507a005ab8fba4402253b0fb43bc3c5bf402d
Paul Duncan <48162083+PDunCanCode@users.noreply.github.com>
2020-03-13 15:30:09 -0700

    Google OAuth Config

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,7 +1,9 @@
 import { MongoClient } from "mongodb";
 import { Database, Booking, Listing, User } from "../lib/types";
 
-const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net`;
+const url = `mongodb+srv://${process.env.DB_USER}:${
+  process.env.DB_USER_PASSWORD
+}@${process.env.DB_CLUSTER}.mongodb.net`;
 
 export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(url, { useNewUrlParser: true });
