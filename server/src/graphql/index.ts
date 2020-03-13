1de7978a80c1d17805f49d2b499056a3d6db4e0d
Paul Duncan <48162083+PDunCanCode@users.noreply.github.com>
2020-03-13 15:28:19 -0700

    Google OAuth Config

--- server/src/graphql/index.ts
+++ server/src/graphql/index.ts
@@ -1,2 +1,4 @@
-export * from './resolvers';
-export * from './typeDefs';
+import merge from "lodash.merge";
+import { viewerResolvers } from "./Viewer";
+
+export const resolvers = merge(viewerResolvers);
