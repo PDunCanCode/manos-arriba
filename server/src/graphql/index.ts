32a246d7535912b7f0c61ed435eaa761757da5f9
Paul Duncan <48162083+PDunCanCode@users.noreply.github.com>
2020-03-17 09:23:06 -0700

    updating typeDefs

--- server/src/graphql/index.ts
+++ server/src/graphql/index.ts
@@ -1,4 +1,2 @@
-import merge from "lodash.merge";
-import { viewerResolvers } from "./Viewer";
-
-export const resolvers = merge(viewerResolvers);
+export * from './resolvers';
+export * from './typeDefs';
